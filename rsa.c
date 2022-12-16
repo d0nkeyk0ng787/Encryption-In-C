d8bd06907f927759635f08df8237166141436349
d0nkeyk0ng787 <memelord253@gmail.com>
2022-12-16 19:46:17 +1100

    First

---
+++ rsa.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+long RandomPrimes()
+{
+    return 0;
+}
+
+long RabinMiller()
+{
+    return 0;
+}
+
+char * Encryptor()
+{
+    long p;
+    long q;
+    long n;
+    int e;
+
+    return "test";
+}
+
+char * Decryptor()
+{
+    return "\ntest2";
+}
+
+int main()
+{
+    char * plainText;
+
+    printf("Enter a string to be encrypted: ");
+    scanf("%s", &plainText);
+
+    // printf(Encryptor());
+    // printf(Decryptor());
+    return 0;
+}
