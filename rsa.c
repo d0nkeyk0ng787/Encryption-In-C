431c54ed2e71e8d70e77594b944bc68b81b42d23
d0nkeyk0ng787 <memelord253@gmail.com>
2022-12-17 12:43:34 +1100

    Generate primes

--- rsa.c
+++ rsa.c
@@ -1,10 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <gmp.h>
 
-long RandomPrimes()
+
+void RandomPrimes(mpz_t* p, mpz_t* q);
+
+int main(void)
 {
+    mpz_t p, q;
+    mpz_init(p);
+    mpz_init(q);
+
+    RandomPrimes(&p, &q);
+
+    gmp_printf("p = %Zd\n", p);
+    gmp_printf("q = %Zd\n", q);
+
+    mpz_clear(p);
+    mpz_clear(q);
+
+    char * plainText;
+
+    printf("Enter a string to be encrypted: ");
+    scanf("%s", &plainText);
+    //printf("%d", RandomPrimes(100000000)); 
+
+    // printf(Encryptor());
+    // printf(Decryptor());
     return 0;
+}
+
+void RandomPrimes(mpz_t* p, mpz_t* q)
+{
+    int bitSize = 2048;
+    gmp_randstate_t state;
+
+    gmp_randinit_default(state);
+
+    mpz_urandomb(p, state, bitSize);
+    mpz_urandomb(q, state, bitSize);
+    mpz_nextprime(p, p);
+    mpz_nextprime(q, q);
+
+    gmp_randclear(state);
 }
 
 long RabinMiller()
@@ -12,13 +51,13 @@ long RabinMiller()
     return 0;
 }
 
+void GenerateKeys()
+{
+    printf("Place holder");
+}
+
 char * Encryptor()
 {
-    long p;
-    long q;
-    long n;
-    int e;
-
     return "test";
 }
 
@@ -27,14 +66,3 @@ char * Decryptor()
     return "\ntest2";
 }
 
-int main()
-{
-    char * plainText;
-
-    printf("Enter a string to be encrypted: ");
-    scanf("%s", &plainText);
-
-    // printf(Encryptor());
-    // printf(Decryptor());
-    return 0;
-}
